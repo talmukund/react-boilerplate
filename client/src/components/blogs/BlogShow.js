04ebc5254c7bab85317a536b653420a866ae5f20
talmukund <bal.mukund015435@gmail.com>
2018-06-10 20:47:23 +0530

    advance node implementation

---
+++ client/src/components/blogs/BlogShow.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchBlog } from "../../actions";
+
+class BlogShow extends Component {
+  componentDidMount() {
+    this.props.fetchBlog(this.props.match.params._id);
+  }
+
+  renderImage() {
+    if (this.props.blog.imageUrl) {
+      return (
+        <img
+          src={
+            "https://s3.amazonaws.com/my-dbatz-bucket-123/" +
+            this.props.blog.imageUrl
+          }
+          alt=""
+        />
+      );
+    }
+  }
+
+  render() {
+    if (!this.props.blog) {
+      return "";
+    }
+
+    const { title, content } = this.props.blog;
+
+    return (
+      <div>
+        <h3>{title}</h3>
+        <p>{content}</p>
+        {this.renderImage()}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ blogs }, ownProps) {
+  return { blog: blogs[ownProps.match.params._id] };
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchBlog }
+)(BlogShow);
